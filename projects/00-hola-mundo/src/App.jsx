5a3d368f2d274ad81d82897be7efbda9cbdd3d66
Giannina <giannisarappa@gmail.com>
2024-03-01 00:58:50 +0100

    Correcciones antes de pushear 2

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import './App.css'
-import { TwitterFollowCard } from './TwitterFollowCard'
+import "./App.css";
+import { TwitterFollowCard } from "./TwitterFollowCard";
 
 export function App() {
-
-  const funcAddUserName = (userName) => `@${userName}`
+  const formattedUserName = <span>@Pachiiii</span>;
 
   return (
-    //Esto era el React.Fragment
-    <section className='App'> 
-      <TwitterFollowCard 
-        placeUserName={funcAddUserName} //paso la fc para que la ejecute en Tw
-        userName={"Scalextric"} 
-        name={"Scalextric"} 
-        isFollowing={false} />
-      <TwitterFollowCard 
-        placeUserName={funcAddUserName} 
-        userName={"pachiFueraDeCarril"} 
-        name={"Pachi Fuera de Carril"} />
+    //Esto era el React.Fragment COMPONENTE
+    <section className="App">
+      <TwitterFollowCard
+        placeUserName={formattedUserName} //paso la fc (pero no la ejecución!! ejecución: fc() ) para que la ejecute en Tw
+        userName={"Scalextric"}
+        name={"Scalextric"}
+        isFollowing={false}
+      />
+      <TwitterFollowCard
+        placeUserName={formattedUserName}
+        userName={"pachiFueraDeCarril"}
+        name={"Pachi Fuera de Carril"}
+        isFollowing
+      />
     </section>
-  )
+  );
 }
-
