8dcfd5702c0aedec2467a9557214071599309607
Giannina <giannisarappa@gmail.com>
2024-03-01 13:11:36 +0100

    Unfollow + Renderizado de listas

--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 export function TwitterFollowCard({ children, userName, initialIsFollowing }) {
   //OPERADOR CONDICIONAL TERNARIO condition ? expr1 : expr2
   const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
-  const text = isFollowing ? "Siguiendo" : "Seguir";
+  const text = isFollowing ? "Following" : "Follow";
   const buttonClassName = isFollowing
     ? "tw-followCard-button is-following"
     : "tw-followCard-button";
@@ -29,7 +29,8 @@ export function TwitterFollowCard({ children, userName, initialIsFollowing }) {
 
       <aside>
         <button className={buttonClassName} onClick={handleClick}>
-          {text}
+          <span className="tw-followCard-text">{text}</span>
+          <span className="tw-followCard-unfollow">Unfollow</span>
         </button>
       </aside>
     </article>
