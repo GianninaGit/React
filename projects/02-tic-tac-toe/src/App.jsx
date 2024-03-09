311e34f05728055699270939dabcf0a4708ca59a
Giannina <giannisarappa@gmail.com>
2024-03-09 10:58:40 +0100

    Manejar empate

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import confetti from "canvas-confetti";
 
 const TURNS = {
   X: "x",
@@ -52,6 +53,16 @@ function App() {
     return null;
   };
 
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  };
+
+  const checkEndGame = (newBoard) => {
+    return newBoard.every((square) => square != null);
+  };
+
   const updateBoard = (index) => {
     if (board[index] || winner) return;
 
@@ -64,13 +75,17 @@ function App() {
 
     const newWinner = checkWinner(newBoard); //si aquí yo le paso el board
     if (newWinner) {
+      confetti();
       setWinner(newWinner); //la actualización de los estados es ASINCRONA, aquí todavía no tiene el nuevo estado, por eso sale null
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false);
     }
   };
 
   return (
     <main className="board">
       <h1>"Tic tac toe"</h1>
+      <button onClick={resetGame}>Reset del juego</button>
       <section className="game">
         {board.map((_, index) => {
           return (
@@ -85,6 +100,20 @@ function App() {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+
+      {winner != null && (
+        <section className="winner">
+          <div className="text">
+            <h2>{winner == false ? "Empate" : "Ganó"}</h2>
+            <header className="win">
+              {winner && <Square>{winner}</Square>}
+            </header>
+            <footer>
+              <button onClick={resetGame}>Empezar de nuevo</button>
+            </footer>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
